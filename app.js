0b81131dbd165c6bd51993ebcb1d9e0e6bbc1a4e
Seba Soto <sebastian.pkfd@gmail.com>
2020-08-07 20:18:47 -0400

    Few interface modifications and basic sum functionality added

--- app.js
+++ app.js
@@ -28,3 +28,33 @@ function operate(a = 0, b = 0, operator){
             break;
     }
 }
+
+let numberButtons = document.querySelectorAll('.button')
+let operation = '';
+numberButtons.forEach( (button) =>{
+    button.addEventListener('click', () => {
+        console.log(button.value);
+        operation += button.value;
+        console.log(operation);
+    });
+})
+
+let sumOperator = document.getElementById('+')
+sumOperator.addEventListener('click', () =>{
+    console.log(sumOperator.id);
+    operation +=sumOperator.id;
+    console.log(operation);
+})
+
+let equalsOperator = document.getElementById('equals');
+equalsOperator.addEventListener('click', ()=>{
+    expresionArray = operation.split('+');
+    console.log(operate(Number(expresionArray[0]), Number(expresionArray[1]), '+'));
+})
+
+//  para usar en el display
+// let display = document.querySelector('.result-container');
+// let displayOperation = document.createElement('div');
+// displayOperation.classList.add('content');
+// displayOperation.textContent = operation.concat(button.value)
+// display.appendChild(displayOperation)
