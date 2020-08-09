c1550ca522c22e927940cb21145ce2be66e263f9
Seba Soto <sebastian.pkfd@gmail.com>
2020-08-08 23:38:01 -0400

    Interface blocked if tries to divide by 0

--- app.js
+++ app.js
@@ -13,11 +13,11 @@ function operate(a = 0, b = 0, operator){
     }
 }
 
-function displayResult(){
+function displayResult(result = '0'){
     let containerResult = document.querySelector('.result-container');
     let contentResult = document.createElement('div');
     contentResult.classList.add('content');
-    contentResult.textContent = operation;
+    contentResult.textContent = result;
     if (containerResult.firstChild === null) {
         containerResult.appendChild(contentResult);
     }else{
@@ -26,7 +26,6 @@ function displayResult(){
     }
 }
 
-// Funcion Auxiliar, después su funcionamiento será enviado a otra funcion
 function resolve(string, operator = '+'){
     let operatorToEvaluate;
     if (string.includes('+')){
@@ -58,29 +57,37 @@ function resolve(string, operator = '+'){
     }
 }
 
-let numberButtons = document.querySelectorAll('.button')
+displayResult();
 let operation = '';
+
+let numberButtons = document.querySelectorAll('.button')
 numberButtons.forEach( (button) =>{
     button.addEventListener('click', () => {
-        console.log(button.value);
-        operation += button.value;
-        displayResult();
+        if (!operation.includes('error')){
+            console.log(button.value);
+            operation += button.value;
+            displayResult(operation);
+        }
     });
 })
 
 let sumOperator = document.querySelectorAll('.operator')
 sumOperator.forEach( (operator) =>{
     operator.addEventListener('click', () =>{
-        console.log(operator.id);
-        (operation.includes('+') || operation.includes('-') || operation.includes('*') || operation.includes('/')) ? resolve(operation, operator.id): operation += operator.id; 
-        displayResult();
+        if (!operation.includes('error')) {
+            console.log(operator.id);
+            (operation.includes('+') || operation.includes('-') || operation.includes('*') || operation.includes('/')) ? resolve(operation, operator.id): operation += operator.id; 
+            displayResult(operation);
+        }
     })
 })
 
 let equalsOperator = document.getElementById('equals');
 equalsOperator.addEventListener('click', ()=> {
-    resolve(operation,'=')
-    displayResult();
+    if (!operation.includes('error')) {
+        resolve(operation,'=')
+        displayResult(operation);
+    }
 })
 
 let cleanButton = document.getElementById('ac');
@@ -89,5 +96,3 @@ cleanButton.addEventListener('click', ()=>{
     displayResult();
 })
 
-
-// Add float point, C option and block when tries to divide by 0
